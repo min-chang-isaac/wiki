52ee1520799d498593a819a8f6268c527f8dc359
minchang <user@DESKTOP-0OPUUR3>
2026-03-11 19:39:06 +0900

    entityおよびrepository

---
+++ src/main/java/com/example/wiki/controller/WikiController.java
@@ -0,0 +1,5 @@
+package com.example.wiki.controller;
+
+public class WikiController {
+
+}
--- src/main/java/com/example/wiki/entity/Wikipage.java
+++ src/main/java/com/example/wiki/entity/Wikipage.java
@@ -1,5 +1,20 @@
 package com.example.wiki.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Entity
 public class Wikipage {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String title;
+	
+	@Lob
+	private String content;
+	
 }
