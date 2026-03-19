fde19725f20567727be0c3916c84e2c8e3fa6d9a
min-chang-isaac <199054631+min-chang-isaac@users.noreply.github.com>
2026-03-20 00:07:27 +0900

    3/20

--- src/main/java/com/example/wiki/controller/WikiController.java
+++ src/main/java/com/example/wiki/controller/WikiController.java
@@ -3,7 +3,9 @@ package com.example.wiki.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.wiki.entity.WikiPage;
 import com.example.wiki.service.WikiService;
 
 @Controller
@@ -21,4 +23,20 @@ public class WikiController {
 		return "index";
 		
 	}
+	
+	@GetMapping("/pages/new")
+	public String newPageForm(Model model) {
+		model.addAttribute("page", new WikiPage());
+		return "new";
+	}
+	
+	@PostMapping("/pages")
+	public String createPage(WikiPage page) {
+		service.save(page); 
+		return "redirect:/";
+	}
+	
+	
+	
+	
 }
